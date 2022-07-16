05ade005f09f4e7033c88f7b3d3dcc33f4ce4e47
Alex <olivesargreek@gmail.com>
2022-07-16 05:25:48 +0300

    fix: Main component

--- blogget/src/components/Main/Main.jsx
+++ blogget/src/components/Main/Main.jsx
@@ -2,10 +2,12 @@
 import Layout from '../Layout';
 import style from './Main.module.css';
 
-export const Main = () => {
+const Main = () => {
   return (
     <main className={style.main}>
       <Layout></Layout>
     </main>
   );
 };
+
+export default Main;
