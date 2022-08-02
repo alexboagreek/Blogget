2ab39bb823e86f9d77f64f2bcea722bc127b5ed7
Alex <olivesargreek@gmail.com>
2022-08-02 15:16:41 +0300

    feat: lesson_8

--- blogget/src/components/Modal/FormComment/FormComment.jsx
+++ blogget/src/components/Modal/FormComment/FormComment.jsx
@@ -1,14 +1,16 @@
 import style from './FormComment.module.css';
-import {useRef} from 'react';
+import {useRef, useContext} from 'react';
+import {authContext} from './../../../context/authContext';
 
 export const FormComment = () => {
+  const {auth} = useContext(authContext);
   const inputRef = useRef(null);
   const handleSubmit = (event) => {
     event.preventDefault();
   };
   return (
     <form className={style.form} onSubmit={handleSubmit}>
-      <h3 size={14} tsize={18}>Имя авторизованного пользователя</h3>
+      <h3 size={14} tsize={18}>{auth.name}</h3>
       <textarea className={style.textarea} ref={inputRef}></textarea>
       <button className={style.btn}>Отправить</button>
     </form>
