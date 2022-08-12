0d935c364789504170c3721e931503ef6dd2defc
Alex <olivesargreek@gmail.com>
2022-08-12 10:00:07 +0300

    feat: lesson_14

--- blogget/src/components/Main/Main.jsx
+++ blogget/src/components/Main/Main.jsx
@@ -6,6 +6,7 @@ import {Routes, Route} from 'react-router-dom';
 import {Modal} from '../Modal/Modal';
 import {Home} from '../../pages/Home/Home';
 import {NotFound} from '../../pages/NotFound/Notfound';
+import {Searched} from '../Searched/Searched';
 
 
 export const Main = () => {
@@ -15,10 +16,12 @@ export const Main = () => {
         <Tabs/>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/auth' element={<Home />} />
           <Route path='*' element={<NotFound/>}/>
           <Route path='/category/:page' element={<List/>}>
             <Route path='post/:id' element={<Modal/>} />
           </Route>
+          <Route path='/searched/:page' element={<Searched />} />
         </Routes>
       </Layout>
     </main>
