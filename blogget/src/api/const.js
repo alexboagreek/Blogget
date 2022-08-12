7087ece680a72b926ee366a76edcf22339a22273
Alex <olivesargreek@gmail.com>
2022-08-12 08:45:34 +0300

    fix: REDIRECT_URI

--- blogget/src/api/const.js
+++ blogget/src/api/const.js
@@ -4,7 +4,7 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'aQLz8miEW0A5T6AN0xATBg';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'blogget-chi.vercel.app';
+export const REDIRECT_URI = 'https://blogget-alexboagreek.vercel.app/auth';
 export const SCOPE = 'identity submit read';
 
 
