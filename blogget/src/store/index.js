620838d6df809b0ede42ca48c66e04230a91e47c
Alex <olivesargreek@gmail.com>
2022-08-12 10:29:11 +0300

    feat: lesson_14

--- blogget/src/store/index.js
+++ blogget/src/store/index.js
@@ -4,7 +4,11 @@ import {authReducer} from './auth/authReducer';
 import postsReducer from './postsData/postsSlice';
 import commentsDataReducer from './commentsData/commentsSlice';
 import {configureStore} from '@reduxjs/toolkit';
+import createSagaMiddleware from '@reduxjs/toolkit';
+import rootSaga from './saga';
+import {searchReducer} from './search/searchReducer';
 
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
@@ -13,9 +17,10 @@ export const store = configureStore({
     authReducer,
     postsReducer,
     commentsDataReducer,
+    searchReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(tokenMiddleware),
+    getDefaultMiddleware().concat(tokenMiddleware, sagaMiddleware),
 });
 
-
+sagaMiddleware.run(rootSaga);
